8f06cb9bd2dcb23ebe9c10c1657ac1be1c742370
yayevy <yayeverest@gmail.com>
2025-07-14 17:00:24 -0400

    a

---
+++ src/main/java/vs/github/evy/valkyriensteampunk/ValkyrienSteampunk.java
@@ -0,0 +1,12 @@
+package vs.github.evy.valkyriensteampunk;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ValkyrienSteampunk.MOD_ID)
+public class ValkyrienSteampunk {
+    public static final String MOD_ID = "valkyrien_steampunk";
+
+    public ValkyrienSteampunk() {
+        // Common Code Here
+    }
+}
